93e64c7571f1c9b01003589d5e6c8a665ee1f45c
Dan Mihai Grigoriciuc <griganu06@gmail.com>
2019-03-11 12:32:30 +0100

    primer commit

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  location: { type: { type: String }, coordinates: [Number] }
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+userSchema.index({ location: '2dsphere' });
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
