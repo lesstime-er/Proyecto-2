75cd3384430c5052c34a773e1df1ee5e56af147a
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos.local>
2019-03-11 17:30:28 +0100

    auth

--- models/User.js
+++ models/User.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String,
-  location: { type: { type: String }, coordinates: [Number] }
+    username: String,
+    password: String,
+    // location: { type: { type: String }, coordinates: [Number] }
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
 });
-userSchema.index({ location: '2dsphere' });
+// userSchema.index({ location: '2dsphere' });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
