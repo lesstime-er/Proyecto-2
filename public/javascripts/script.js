512fd4b6762d99c09e4c877509cb41238eed9c2e
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos.local>
2019-03-11 14:41:57 +0100

    first step

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
-
+    console.log('IronGenerator JS imported successfully!');
+    console.dir(document.getElementById("check"))
+    document.getElementById("check").addEventListener("click", (e) => {
+        console.log("entra")
+            //axios.get("d")
+            //.then(res => console.Console(res.data))
+    })
 }, false);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const axios = require("axios")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+    axios.get("https://datos.madrid.es/egob/catalogo/212769-0-atencion-medica.json")
+        .then(response => {
+            console.log(response.data["@graph"].forEach((el) => {
+                //let hospital = /^hospital$/
+                if (el.title.startsWith('H')) console.log(el.title, el.location)
+            }));
+            res.render('index');
+        })
 });
 
-module.exports = router;
+module.exports = router
